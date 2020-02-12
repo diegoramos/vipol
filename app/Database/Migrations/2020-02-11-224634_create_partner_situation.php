07e29b985cc40d12d0c667cefe69c78c37b43e86
Richard Diego <diego_lider@hotmail.com>
2020-02-11 20:13:56 -0500

    segundo commit

---
+++ app/Database/Migrations/2020-02-11-224634_create_partner_situation.php
@@ -0,0 +1,55 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePartnerSituation extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'null'           => FALSE,
+				//'unsigned'       => TRUE,
+				//'auto_increment' => TRUE
+			],
+			'description_short'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'description_long' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '200',
+			],
+			'created_at' => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'updated_at' => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'deleted_at'       	=> [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'deleted'       	=> [
+	            'type'           => 'INT',
+	            'constraint'     => '1',
+	            'null'           => FALSE,
+	            'default'		 =>	'0',
+	        ]
+		]);
+
+	    $this->forge->addKey('id', TRUE);
+	    $this->forge->createTable('partner_situation');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('partner_situation');
+	}
+}
---
+++ app/Database/Migrations/2020-02-11-231848_create_identification_document.php
@@ -0,0 +1,53 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateIdentificationDocument extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 5,
+				'null'           => FALSE,
+			],
+			'description_short'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'description_long' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '200',
+			],
+			'created_at' => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'updated_at' => [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'deleted_at'       	=> [
+				'type'           => 'DATETIME',
+				'null'           => TRUE,
+			],
+			'deleted'       	=> [
+	            'type'           => 'INT',
+	            'constraint'     => '1',
+	            'null'           => FALSE,
+	            'default'		 =>	'0',
+	        ]
+		]);
+
+	    $this->forge->addKey('id', TRUE);
+	    $this->forge->createTable('identification_document');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('identification_document');
+	}
+}
