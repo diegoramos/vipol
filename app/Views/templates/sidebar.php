583fe3d2a291b48e4f7f037b5de9aae79176f639
Richard Diego <diego_lider@hotmail.com>
2020-02-14 01:09:08 -0500

    ok

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="<?php echo base_url();?>vendor/almasaeed2010/adminlte/dist/img/logo_vipol.png" alt="Vipol Logo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url();?>assets/adminlte/dist/img/logo_vipol.png" alt="Vipol Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">VIPOL</span>
     </a>
@@ -12,7 +12,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?php echo base_url();?>vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php echo base_url();?>assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
